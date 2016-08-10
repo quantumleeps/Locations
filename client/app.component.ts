32d1767f2bc693eea0b11c26cdd0e4bb9b9118be
quantumleeps <dan.leeper@gmail.com>
2016-08-10 19:15:05 -0400

    Step 3.3: Add Locations collection

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
- 
+import { Locations } from '../both/collections/locations.collection';
+import { Mongo } from 'meteor/mongo';
+
 import template from './app.component.html';
 
 @Component({
   selector: 'app',
   template
 })
-export class AppComponent {}
+export class AppComponent {
+
+  locations: Mongo.Cursor<any>;
+
+  constructor () {
+    this.locations = Locations.find();
+  }
+
+}
+
+
+// this.locations = [
+    //   {name: "Blue Hills", region: "Nassau", country: "Bahamas"},
+    //   {name: "Northside Waterworks (NSWW)", region: "West End - Grand Cayman", country: "Cayman Islands"}
+    // ];
