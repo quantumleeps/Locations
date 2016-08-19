edbd0f235a1857fe4ff8354bc0e9aaeeb1ff2822
quantumleeps <dan.leeper@gmail.com>
2016-08-19 07:42:02 -0400

    Change routing to include home splash page

--- client/app.routes.ts
+++ client/app.routes.ts
@@ -1,10 +1,12 @@
 import { RouterConfig, provideRouter } from '@angular/router';
 
+import { HomeComponent } from './imports/home/home.component';
 import { LocationsList } from './imports/locations/locations-list.component';
 import { LocationDetails } from './imports/locations/location-details.component';
 import { DataGroupDetails } from './imports/data-groups/data-group-details.component';
 
 const routes: RouterConfig = [
+    { path: '', component: HomeComponent },
     { path: 'configure', component: LocationsList },
     { path: 'configure/location/:locationId', component: LocationDetails },
     { path: 'configure/location/:locationId/data-group/:dataGroupId', component: DataGroupDetails }
--- client/imports/home/home.component.ts
+++ client/imports/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import template from './home.component.html';
-@Component({
 
+@Component({
+    selector: 'home',
+    template,
+    directives: [ROUTER_DIRECTIVES]
 })
 
 export class HomeComponent {
