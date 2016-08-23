2472aa4b67fc24cfaceae4c9e1bd2ac4ad5242b1
Dan Leeper <dan.leeper@gmail.com>
2016-08-23 13:09:09 -0400

    Add style to splash page

--- client/imports/home/home.component.ts
+++ client/imports/home/home.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import { ROUTER_DIRECTIVES, Router } from '@angular/router';
 
 import template from './home.component.html';
 
 @Component({
     template,
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES],
+    styles: [`
+
+    .clickbox {
+        border: 1px solid black;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        text-align: center;
+    }
+    .clickbox a {
+        display: block;
+        width: 100%;
+        padding: 20px;
+    }
+
+    .page-title {
+        font-size: 30px;
+    }
+    `]
 })
 
 export class HomeComponent {
-
+    test() {
+        console.log("testingthisthingfuckfuck")
+    }
 }
