446232df0bf44f1623f6100a54a2f6ac3ebc86ff
Dan Leeper <dan.leeper@gmail.com>
2016-08-23 11:34:27 -0400

    Add homepage and link to configure

--- client/imports/home/home.component.ts
+++ client/imports/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import template from './home.component.html';
 
 @Component({
-    template
+    template,
+    directives: [ROUTER_DIRECTIVES]
 })
 
 export class HomeComponent {
