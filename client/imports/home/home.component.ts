e7ca85d74b8b39b6080f6e2fad615d7c9b2d54be
quantumleeps <dan.leeper@gmail.com>
2016-08-23 22:27:57 -0400

    Separate styles from component

--- client/imports/home/home.component.ts
+++ client/imports/home/home.component.ts
@@ -5,39 +5,7 @@ import template from './home.component.html';
 
 @Component({
     template,
-    directives: [ROUTER_DIRECTIVES],
-    styles: [`
-
-    html, body {
-        height: 100vh;
-    }
-
-    .wrapper {
-        background-color: #25677D;
-        height: 100vh;
-    }
-
-    .clickbox {
-        border: 2px solid black;
-        margin-top: 10px;
-        margin-bottom: 10px;
-        text-align: center;
-        background-color: white;
-        width: 95vw;
-    }
-    .clickbox a {
-        display: block;
-        width: 100%;
-        padding: 20px;
-        font-size: 100px;
-        color: #009BCF;
-    }
-
-    .page-title {
-        font-size: 70px;
-        color: white;
-    }
-    `]
+    directives: [ROUTER_DIRECTIVES]
 })
 
 export class HomeComponent {
