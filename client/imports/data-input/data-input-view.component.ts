842cc50c347abc62f86d53c7d122672ecf023054
Dan Leeper <dan.leeper@gmail.com>
2016-08-25 16:02:45 -0400

    Add sorting mechanism inside add-data record for data-groups

--- client/imports/data-input/data-input-view.component.ts
+++ client/imports/data-input/data-input-view.component.ts
@@ -5,6 +5,7 @@ import { MeteorComponent } from 'angular2-meteor';
 
 import { DataPoint } from '../../../both/interfaces/data-point.interface';
 import { DataPoints } from '../../../both/collections/data-points.collection';
+import { DataGroups } from '../../../both/collections/data-groups.collection';
 import { Locations } from '../../../both/collections/locations.collection';
 import { CollectedData } from '../../../both/collections/collected-data.collection';
 
@@ -27,6 +28,8 @@ export class DataInputView extends MeteorComponent implements OnInit {
     curLocationName: any;
     curRecord: any;
     dataPoints: any;
+    dataGroups: any;
+    curDataGroup: any;
 
     constructor(private router: Router, private route: ActivatedRoute) {
         super();
@@ -40,37 +43,57 @@ export class DataInputView extends MeteorComponent implements OnInit {
             .subscribe(dataInputId => {
                 this.dataInputId = dataInputId;
 
-                this.subscribe('collected-data-record', this.dataInputId, () => {
-                    this.curRecord = CollectedData.findOne(this.dataInputId);
-                    this.curLocationId = this.curRecord.locationId;
-
-                    this.subscribe('location', this.curLocationId, () => {
-                        this.curLocation = Locations.findOne(this.curLocationId);
-                        // this.curLocationName = this.curLocation.name;
-                        this.curLocationName = this.curLocation.name;
-                    }, true);
-
-
-                }, true);
-
 
             });
 
         //need the name of the current location
 
+        this.subscribe('collected-data-record', this.dataInputId, () => {
+            this.curRecord = CollectedData.findOne(this.dataInputId);
+            this.curLocationId = this.curRecord.locationId;
+
+            this.subscribe('location', this.curLocationId, () => {
+                this.curLocation = Locations.findOne(this.curLocationId);
+                this.curLocationName = this.curLocation.name;
+
+            });
+
+
+            this.subscribe('data-groups', () => {
+                var temp = DataGroups.find({ "locationId": this.curLocationId })
+                this.dataGroups = temp.map(function (a) {
+                    return a
+                })
+                this.curDataGroup = this.dataGroups[0]._id
+
+                this.subscribe('data-points', () => {
+                    this.dataPoints = DataPoints.find({ "locationId": this.curLocationId, "dataGroupId": this.curDataGroup })
+                    // this.dataPoints = DataPoints.find({ "locationId": this.curLocationId })
+                })
+
+            })
+
+        });
 
 
         //need the list of datagroups
 
-        this.subscribe('data-points', () => {
-            // this.dataPoints = DataPoints.find({ "locationId": this.curLocation, "dataGroupId": this.curDataGroup })
-            this.dataPoints = DataPoints.find({ "locationId": this.curLocationId })
-        })
+
+
 
     }
 
     cancelEntry() {
         this.router.navigate(['/add-data']);
+    }
+
+    changeCurGroup(id) {
+        this.curDataGroup = id;
+
+        this.subscribe('data-points', () => {
+            this.dataPoints = DataPoints.find({ "locationId": this.curLocationId, "dataGroupId": this.curDataGroup })
+        })
+
     }
 
 }
